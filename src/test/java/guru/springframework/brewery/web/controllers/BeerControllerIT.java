c3338d4e4b038c1665eecc9c63bcaa751566ff42
kdlug <casi@o2.pl>
2020-03-22 11:34:55 +0100

    Using Test RestTemplate

---
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerIT.java
@@ -0,0 +1,23 @@
+package guru.springframework.brewery.web.controllers;
+
+import guru.springframework.brewery.web.model.BeerPagedList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BeerControllerIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testListBeers() {
+        BeerPagedList beerPageList = restTemplate.getForObject("/api/v1/beer", BeerPagedList.class);
+        assertThat(beerPageList.getContent()).hasSize(3);
+
+    }
+}
